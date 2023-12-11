397bf03eea296184a6baf4cd62e113683844998c
Joshua <jbalansa143@gmail.com>
2023-12-11 22:10:06 +0800

    posts rest api

--- api/post.php
+++ api/post.php
@@ -3,7 +3,9 @@
 include "../function.php";
 require "../db-connect.php";
 
-
+/**
+ * Post rest API
+ */
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method === 'GET') {
@@ -25,7 +27,9 @@ if($method === 'GET') {
             "error" => "Missing required fields"
         ));
     } else {
-        $stmt = $connect->prepare("INSERT INTO posts (title, description, phone, email, address, image) VALUES (?, ?, ?, ?, ?, ?)");
+
+        $sql = "INSERT INTO posts (title, description, phone, email, address, image) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $connect->prepare($sql);
         $stmt->bind_param("ssssss", $data['title'], $data['description'], $data['phone'], $data['email'], $data['address'], $data['image']);
 
         if ($stmt->execute()) {
@@ -38,6 +42,37 @@ if($method === 'GET') {
             ));
         }
     }
+} elseif ($method == 'PUT') {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if (empty($data['title']) || empty($data['description']) || empty($data['phone']) || empty($data['email']) || empty($data['address']) || empty($data['image'])) {
+        returnJson(array(
+            "error" => "Missing required fields"
+        ));
+    } else {
+      
+        $sql = "UPDATE posts SET title = ?, description = ?, phone = ?, email = ?, address = ?, image = ? WHERE id = ?";
+        $stmt = $connect->prepare($sql);
+        $stmt->bind_param('ssssssi', $data['title'], $data['description'], $data['phone'], $data['email'], $data['address'], $data['image'], $data['id']);
+
+        if ($stmt->execute()) {
+            returnJson(array(
+                "message" => "Data updated successfully"
+            ));
+        } else {
+            returnJson(array(
+                "message" => "Failed to update data"
+            ));
+        }
+    }
+} elseif ($method === 'DELETE') {
+   
+    $data = json_decode(file_get_contents("php://input"), true);
+    $stmt = $connect->prepare("DELETE FROM posts WHERE id = ?");
+    $stmt->bind_param("i", $data['id']);
+    $stmt->execute();
+
+    echo json_encode(array("message" => "Data deleted successfully"));
 }
 
 $connect->close();
