3587489571683290a1b9d4cd9813a632046715a7
Joshua <jbalansa143@gmail.com>
2023-12-13 22:16:46 +0800

    allow x-origin

--- function.php
+++ function.php
@@ -4,6 +4,9 @@ include 'db-connect.php';
 function returnJson($data)
 {
 	header("Content-Type: application/json");
+	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400'); 
 	echo json_encode($data);
 	exit();
 }
